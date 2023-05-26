62f5958cbbf92987cae25865bd616150f9d91fdf
vagner santos <vagner131417@gmail.com>
2023-05-26 15:18:27 -0300

    feat: styles types

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+  body {
+    background: ${(props) => props.theme.colors["base-background"]};
+    color: ${(props) => props.theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, textarea, button {
+    font-family: ${(props) => props.theme.fonts.regular};
+    font-weight: 400;
+    font-size: ${(props) => props.theme.textSizes["text-bold-m"]}
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
