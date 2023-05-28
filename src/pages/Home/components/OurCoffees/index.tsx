47a46b927822e20ef3fe1ee32fa6db4f746810fb
vagner santos <vagner131417@gmail.com>
2023-05-28 18:11:03 -0300

    feat: create cads menu fixed

--- src/pages/Home/components/OurCoffees/index.tsx
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -1,6 +1,21 @@
+import { TitleText } from "../../../../components/Typography";
+import { coffees } from "../../../../data/coffees";
+import { CoffeeCard } from "../CoffeeCard";
+import { CoffeeList, OurCoffeeContainer } from "./styles";
 
 export function OurCoffees() {
-  return(
-    <p>coffee</p>
-  )
+  return (
+    <OurCoffeeContainer className="container">
+      <TitleText size="l" color="subtitle">
+        Nossos cafés
+      </TitleText>
+
+      <CoffeeList>
+        {coffees.map((coffee) =>(
+          <CoffeeCard key={coffee.id} coffee={coffee} />
+
+        ))}
+      </CoffeeList>
+    </OurCoffeeContainer>
+  );
 }
